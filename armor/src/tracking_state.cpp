6374ee47619077d888b73dba97cd0dd47fc97482
sycskr <55191314+sycskr@users.noreply.github.com>
2020-08-21 11:09:19 +0800

    kcf tracking 注释添加

--- armor/src/tracking_state.cpp
+++ armor/src/tracking_state.cpp
@@ -7,17 +7,24 @@
 #include <armor_finder/armor_finder.h>
 #include <show_images/show_images.h>
 
+/**
+ * @name ArmorFinder::stateTrackingTarget
+ * @param cv::Mat &src 当前图像
+ * @func 根据上一帧目标的位置得出这一帧的目标
+ * @return bool 追踪失败，返回false。
+*/
 bool ArmorFinder::stateTrackingTarget(cv::Mat &src) {
     auto pos = target_box.rect;
     if(!tracker->update(src, pos)){ // 使用KCFTracker进行追踪
-        target_box = ArmorBox();
+        target_box = ArmorBox();//?????????????????????????????????
         LOGW("Track fail!");
-        return false;
+        return false;//追踪失败
     }
+    //Rect2d(0, 0, 640, 480) x,y,width,height
     if((pos & cv::Rect2d(0, 0, 640, 480)) != pos){
-        target_box = ArmorBox();
+        target_box = ArmorBox();//?????????????????????????????????
         LOGW("Track out range!");
-        return false;
+        return false;//超出范围
     }
 
     // 获取相较于追踪区域两倍长款的区域，用于重新搜索，获取灯条信息
@@ -30,11 +37,16 @@ bool ArmorFinder::stateTrackingTarget(cv::Mat &src) {
     cv::Mat roi = src(bigger_rect).clone();
 
     ArmorBox box;
-    // 在区域内重新搜索。
+
+    //由于KCFTracker在追踪的时候，只改变追踪框的位置，而不改变长宽。但在后续数据发送时需要长宽信息进行距离的估算。
+    //并且由于KCF算法是根据上一帧更新下一帧，所以当追踪时间长了，可能会忘记一开始的目标追到其他地方去。
+    //所以在区域内重新搜索。
     if(findArmorBox(roi, box)) { // 如果成功获取目标，则利用搜索区域重新更新追踪器
         target_box = box;
-        target_box.rect.x += bigger_rect.x; //　添加roi偏移量
+        //target_box　添加roi偏移量
+        target_box.rect.x += bigger_rect.x; 
         target_box.rect.y += bigger_rect.y;
+        //blob　添加roi偏移量
         for(auto &blob : target_box.light_blobs){
             blob.rect.center.x += bigger_rect.x;
             blob.rect.center.y += bigger_rect.y;
@@ -46,7 +58,7 @@ bool ArmorFinder::stateTrackingTarget(cv::Mat &src) {
         if(classifier){ // 分类器可用，使用分类器判断。
             cv::resize(roi, roi, cv::Size(48, 36));
             if(classifier(roi) == 0){
-                target_box = ArmorBox();
+                target_box = ArmorBox();//?????????????????????????????????
                 LOGW("Track classify fail range!");
                 return false;
             }
@@ -54,9 +66,10 @@ bool ArmorFinder::stateTrackingTarget(cv::Mat &src) {
             cv::Mat roi_gray;
             cv::cvtColor(roi, roi_gray, CV_RGB2GRAY);
             cv::threshold(roi_gray, roi_gray, 180, 255, cv::THRESH_BINARY);
+            //cv::countNonZero返回灰度值不为0的像素数,可用来判断图像是否全黑,此处寻找亮点数
             contour_area = cv::countNonZero(roi_gray);
             if(abs(cv::countNonZero(roi_gray) - contour_area) > contour_area * 0.3){
-                target_box = ArmorBox();
+                target_box = ArmorBox();//?????????????????????????????????
                 return false;
             }
         }
